e7d72230afcdc83e3b4532c11c80369d1d686af9
tj369369 <1512594375@qq.com>
2024-05-21 15:55:39 +0800

    初始化

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @Author：tj
+ * @Package：
+ * @Project：${PROJECT_NAME}
+ * @name：${NAME}
+ * @Date：${DATE} ${TIME}
+ * @Filename：${NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! 你好");
+    }
+}
