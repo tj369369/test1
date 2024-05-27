2cbd39ba4c2a62e081a9bf611f6d2a067993054f
tj369369 <1512594375@qq.com>
2024-05-27 16:37:42 +0800

    baranch2224444

---
+++ src/branch1.java
@@ -0,0 +1,13 @@
+/**
+ * @Author：tj
+ * @Package：PACKAGE_NAME
+ * @Project：git-test1
+ * @name：branch1
+ * @Date：2024/5/23 16:03
+ * @Filename：branch1
+ */
+public class branch1 {
+    public static void main(String[] args) {
+        System.out.println("baranch2224444");
+    }
+}
